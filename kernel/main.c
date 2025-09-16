52ee0143d24d80d8f2a2a76e6ed567d4b08598ea
wnw231423 <2684722187@qq.com>
2025-09-16 18:27:30 +0800

    lab01 ok maybe

--- kernel/main.c
+++ kernel/main.c
@@ -1,10 +1,16 @@
 #include "defs.h"
 
+volatile static int started = 0;
+
 void main() {
     if (cpuid() == 0) {
         uartinit();
         printfinit();
+        printf("hart %d starting\n", cpuid());
+        started = 1;
     } else {
+        while(started == 0)
+            ;
         __sync_synchronize();
         printf("hart %d starting\n", cpuid());
     }
