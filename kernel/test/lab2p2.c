d47695fc2f1cdfaf996dbb08157540e04acdc436
wnw231423 <2684722187@qq.com>
2025-10-06 21:30:40 +0800

    little modify

--- kernel/test/lab2p2.c
+++ kernel/test/lab2p2.c
@@ -17,14 +17,15 @@ void lab2p2()
 
         printf("\ntest-1\n\n");    
         vm_mappages(test_pgtbl, 0, PGSIZE, mem[0], PTE_R);
-        //vm_mappages(test_pgtbl, PGSIZE * 10, PGSIZE / 2, mem[1], PTE_R | PTE_W);
-        //vm_mappages(test_pgtbl, PGSIZE * 512, PGSIZE - 1, mem[2], PTE_R | PTE_X);
+        //vm_mappages(test_pgtbl, PGSIZE * 10, PGSIZE / 2, mem[1], PTE_R | PTE_W);  //size未按页对齐
+        //vm_mappages(test_pgtbl, PGSIZE * 512, PGSIZE - 1, mem[2], PTE_R | PTE_X); //size未按页对齐
+        vm_mappages(test_pgtbl, PGSIZE * 512, PGSIZE, mem[2], PTE_R | PTE_X);
         vm_mappages(test_pgtbl, PGSIZE * 512 * 512, PGSIZE, mem[2], PTE_R | PTE_X);
         vm_mappages(test_pgtbl, MAXVA - PGSIZE, PGSIZE, mem[4], PTE_W);
         vm_print(test_pgtbl);
 
         printf("\ntest-2\n\n");    
-        //vm_mappages(test_pgtbl, 0, PGSIZE, mem[0], PTE_W);
+        //vm_mappages(test_pgtbl, 0, PGSIZE, mem[0], PTE_W);  //Remap
         vm_unmappages(test_pgtbl, PGSIZE * 10, PGSIZE, 1);
         vm_unmappages(test_pgtbl, PGSIZE * 512, PGSIZE, 1);
         vm_print(test_pgtbl);
